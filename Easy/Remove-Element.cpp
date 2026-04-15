c4e193a1437664e918450b422f89e152432c37cd
VGravius <vg.gravius@gmail.com>
2026-04-15 22:05:54 +0300

    Add Remove Element solution (Easy)

---
+++ Easy/Remove-Element.cpp
@@ -0,0 +1,17 @@
+// Link: https://leetcode.com/problems/remove-element
+// Problem: Remove Element
+// Difficulty: Easy
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int index = 0;
+        for (int i = 0; i < nums.size(); i++){
+            if (nums[i] != val){
+                   nums[index] = nums[i];
+                    index++;
+            }
+        }
+        return index;
+    }
+};
