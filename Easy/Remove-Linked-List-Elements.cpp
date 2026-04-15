5c40bf3aedc45b65e74e74c0d34d3de6f30734cf
VGravius <vg.gravius@gmail.com>
2026-04-15 21:50:36 +0300

    Add Remove Linked List Elements solution (Easy)

---
+++ Easy/Remove-Linked-List-Elements.cpp
@@ -0,0 +1,34 @@
+// Link: https://leetcode.com/problems/remove-linked-list-elements
+// Problem: Remove Linked List Elements
+// Difficulty: Easy
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        while (head != nullptr && head->val == val) {
+            head = head->next;
+        }
+        if (head == nullptr) {
+            return nullptr;
+        }
+        ListNode* current = head;
+        while (current->next != nullptr) {
+            if (current->next->val == val) {
+                current->next = current->next->next;
+            } else {
+                current = current->next;
+            }
+        }
+        return head;
+    }
+};
